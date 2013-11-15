f9cb56c7cd8e21efba828035f1be6a35f89fe168
froginvasion <lippens.m@gmail.com>
2013-11-15 21:27:59 +0100

    Update formula.sql
    failes when inserted; so this should hopefully work better :)

--- formula.sql
+++ formula.sql
@@ -1,4 +1,4 @@
-INSERT INTO formula VALUES (0, "Big Stand", 900, "biggest");
-INSERT INTO formula VALUES (1, "Small Stand", 650, "smaller");
-INSERT INTO formula VALUES (2, "Walk by table", 300, "A table at the walk by smallest but present");
-INSERT INTO formula VALUES (3, "Resume book and companies book", 100, "Just advertisment space");
+INSERT INTO formula VALUES (1, "Big Stand", 900, "biggest");
+INSERT INTO formula VALUES (2, "Small Stand", 650, "smaller");
+INSERT INTO formula VALUES (3, "Walk by table", 300, "A table at the walk by smallest but present");
+INSERT INTO formula VALUES (4, "Resume book and companies book", 100, "Just advertisment space");
