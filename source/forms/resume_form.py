c5883f3a9163cceb3bdd92208336e60a68cf9e0a
Spartam <kwpardon@infogroep.be>
2013-11-20 01:00:16 +0100

    fixes #26

--- source/forms/resume_form.py
+++ source/forms/resume_form.py
@@ -9,7 +9,7 @@ class resume_form(Form):
 			raise ValidationError('File must be a pdf')
 
 	enrollment_number = StringField('Enrollment number', validators=[validators.input_required(message='Your enrollment number is required'),\
-																	 validators.regexp('^[0-9]{5,6}', message='Your enrollment number is a number of 5 to 6 digits')])
+																	 validators.regexp('^[0-9]{5,6}$', message='Your enrollment number is a number of 5 to 6 digits')])
 	field_of_study = SelectField('Field of study', choices=config.fields_of_study)
 	resume = FileField('Upload resume', validators=[validators.input_required(message='You need a resume to upload')])
 
