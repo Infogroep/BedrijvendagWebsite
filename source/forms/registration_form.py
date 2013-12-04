c13206822b8851975c421889d8f47bc09a958d42
Spartam <kwpardon@infogroep.be>
2013-12-04 12:48:37 +0100

    edited the postal code check accepts now characters

--- source/forms/registration_form.py
+++ source/forms/registration_form.py
@@ -14,12 +14,18 @@ class RegistrationForm(Form):
 		if company:
 			raise ValidationError("An account for this company already exists")
 
+	def validate_postal(form, field):
+		postal_pattern = re.compile('(?i)^[0-9]{4},? [a-z]*', flags=re.VERBOSE)
 
-	company_name = StringField('Company name', validators=[validators.input_required(message="company name is required")])
+		if postal_pattern.match(field.data) is None:
+			raise ValidationError("Your postal code seems te be incorrect. 1050, 5374 RE")
+
+
+	company_name = StringField('Company name', validators=[validators.input_required(message="Company name is required")])
 	password = PasswordField('Password', validators=[validators.input_required(message="Password is required")])
 	retype_password = PasswordField('Retype password', validators=[validators.input_required(message="Retype password is required"), validators.EqualTo("password", message="Passwords do not match")])
 	address = StringField('Address', validators=[validators.input_required(message="Address is required")])
-	postal = IntegerField('Postal', validators=[validators.input_required(message="Zipcode is required. It may only consist of number")])
+	postal = StringField('Postal', validators=[validators.input_required(message="Postal code is required.")])
 	city = StringField('City', validators=[validators.input_required(message="City is required")])
 	country = StringField('Country', validators=[validators.input_required(message="Country is required")])
 	contact = StringField('Contact person', validators=[validators.input_required(message="Contact is required")])
@@ -28,6 +34,4 @@ class RegistrationForm(Form):
 	tel = StringField('Telephone number', validators=[validators.required(message="Telephone number is required, numbers only"), validators.Length(min=9, max=12, message="Length doesn't match telephone number length")])
 	fax = StringField('Fax number', validators=[validators.optional(), validators.Length(min=9, max=12, message="Length doesn't match fax number length")])
 	cel = StringField('Cellphone number', validators=[validators.optional(), validators.Length(min=9, max=12, message="Length doesn't match cellphone number length")])
-
-
 
