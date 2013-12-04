51b0a61e0403cda23a0075d3d6d39cb5af59d959
Spartam <kwpardon@infogroep.be>
2013-12-04 08:21:35 +0100

    Ayrton had some complaints

--- source/mailing.py
+++ source/mailing.py
@@ -53,16 +53,16 @@ def send_confirmation_mail(to):
 	text = """Beste,
 
 
-Bij deze bevestigen wij uw inschrijving voor de Bedrijvendag 2014. Je steeds een logo uploaden voor uw bedrijf. Indien je dit nog niet hebt gedaan raden wij u ten zeerste aan dit alsnog te doen.
+Bij deze bevestigen wij uw inschrijving voor de Bedrijvendag 2014. Je kunt steeds een logo uploaden voor uw bedrijf. Indien je nog geen logo op de website hebt geplaatst raden wij u ten zeerste aan dit te alsnog doen.
 
-De "bedrijvendagboek" pagina(s) kan nog altijd aangepast worden om te passen in de daarvoor voorziene ruimte. De beschikbare ruimte hangt af van uw formule. Je kan dit altijd controleren op http://bedrijvendag.infogroep.be/pricelist. Je kan ook steeds ruimte bij kopen zolang je het contract niet hebt aangevraagd
+De "bedrijvendagboek" pagina(s) kan/kunnen nog altijd aangepast worden om te passen in de daarvoor voorziene ruimte. De beschikbare ruimte hangt af van uw formule. Je kan dit altijd controleren op http://bedrijvendag.infogroep.be/pricelist. Je kan ook steeds ruimte bijkopen zolang je het contract niet hebt aangevraagd.
 
 Vergeet zeker niet om een contract aan te vragen, zodat we de inschrijving definitief kunnen maken.
 
 We zijn altijd beschikbaar voor vragen en opmerkingen op bedrijvendag@infogroep.be
 
 
-Bedankt voor uw interesse en tot de Bedrijvendag,
+Bedankt voor uw interesse en tot op de Bedrijvendag,
 
 
 Met vriendelijke groeten,
@@ -76,16 +76,16 @@ ENGLISH VERSION
 
 Dear,
 
-Your companies registration for Bedrijvendag 2014. You can always upload your companies logo. We recommend to upload your companies logo if you haven't done this yet.
+We hereby conform your company's registration for Bedrijvendag 2014. You can always upload your company's logo. We recommend you to upload your company's logo if you haven't done this yet.
 
-The "bedrijvendagboek" (companies book) pages can once uploaded. Always been edited by the organisation to fit in the space available to you. This depends on your formula which are visible at http://bedrijvendag.infogroep.be/pricelist. You can also buy more space untill your contract has been requested.
+The "bedrijvendagboek" (companies book) pages can always be edited by the organisation to fit in the space available to you. This depends on your formula which is visible at http://bedrijvendag.infogroep.be/pricelist. You can also buy more space untill your contract has been requested.
 
-Don't forget to request your contract to finish the registration and make it definitive.
+Don't forget to request your contract to complete the registration and make it final.
 
 We are always available for questions and remarks at bedrijvendag@infogroep.be
 
 
-Thank you for your interest and untill the Bedrijvendag,
+Thank you for your interest and hoping to welcome you at the Bedrijvendag.
 
 
 Best regards,
@@ -107,4 +107,3 @@ Elyn Meert
 	s.sendmail(me, to_addresses, msg.as_string())
 	s.quit()
 
-	
